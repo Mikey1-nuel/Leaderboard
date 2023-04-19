28817559f16925f94101e0f7403c4f3f5730ea0a
Emmanuel <emmanuelnwoye12@gmail.com>
2023-04-19 15:48:09 +0100

    Update js files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 import './style.css';
 
-const AddScore = require('./modules/addscore.js');
 const DisplayScores = require('./modules/displayscore.js');
 
-document.addEventListener('DOMContentLoaded', AddScore.nameAndScore);
 document.addEventListener('DOMContentLoaded', DisplayScores.scoreList);
+
+const form = document.getElementById('form');
+const refresh = document.querySelector('.btn');
+
+refresh.addEventListener('click', DisplayScores.scoreList);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const userName = document.getElementById('name').value;
+  const userScore = document.getElementById('score').value;
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/En2iENvSMzzZDyKRElz6/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      user: userName,
+      score: userScore,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=utf-8',
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      throw new Error(`Incorrect HTTP! status: ${error}`);
+    });
+  form.reset();
+});
